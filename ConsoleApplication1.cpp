6db6bfe4f13f630212dd1a177828e25451057889
Егор <98447700+pyger72@users.noreply.github.com>
2022-10-30 17:21:39 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	for (int i = 2; i > sqrt(a); i++) {
+		if (a % i == 0) {
+			cout << "ne prostoe";
+			return 0;
+		}
+		cout << "простое";
+
+
+	}
+	cout << "postoe";
+	return 0;
+}
